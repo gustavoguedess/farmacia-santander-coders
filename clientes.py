4e08a17ec31dd94830f91f70755479dc0ab8599f
danilojpfreitas <danilojpfreitas@gmail.com>
2023-08-15 20:53:09 -0300

    Fix: ID (Laboratorios) and Getters/Setters

--- clientes.py
+++ clientes.py
@@ -1,70 +1,87 @@
+import json
+
 class Cliente:
     def __init__(self, cpf: str, nome: str, sobrenome: str, data_nascimento: str):
-        self.cpf: str = cpf       
-        self.nome: str = nome
-        self.sobrenome: str = sobrenome
-        self.data_nascimento: str = data_nascimento
+        self.cpf: str = cpf #PK      
+        self._nome: str = nome
+        self._sobrenome: str = sobrenome
+        self._data_nascimento: str = data_nascimento
         self.add_cliente()
 
     @property
     def dados_cliente(self):
         return {
             "cpf": self.cpf,
-            "nome": self.nome,
-            "sobrenome": self.sobrenome,
-            "data de nascimento": self.data_nascimento
+            "nome": self._nome,
+            "sobrenome": self._sobrenome,
+            "data de nascimento": self._data_nascimento
         }
     
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+    @property
+    def sobrenome(self):
+        return self._sobrenome
+
+    @sobrenome.setter
+    def sobrenome(self, valor):
+        self._sobrenome = valor
+
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+
+    @data_nascimento.setter
+    def data_nascimento(self, valor):
+        self._data_nascimento = valor        
+    
     def read_cliente(self):
-        dic_cliente = {}
+        cliente = {}
         try:
-            with open("dic_clientes.txt", "r") as file:
-                dic_cliente = eval(file.read())
-            return dic_cliente
+            with open("clientes.json", "r") as file:
+                clientes = json.load(file)
+            return clientes
         except FileNotFoundError:
-            raise "Arquivo dic_clientes.txt não encontrado."
+            raise "Arquivo clientes.json não encontrado."
 
     def add_cliente(self):
         if self.cpf.isnumeric():
-            dic_cliente = self.read_cliente()
-            dic_cliente[self.cpf] = {
-                "nome": self.nome,
-                "sobrenome": self.sobrenome,
-                "data de nascimento": self.data_nascimento
+            clientes = self.read_cliente()
+            clientes[self.cpf] = {
+                "nome": self._nome,
+                "sobrenome": self._sobrenome,
+                "data de nascimento": self._data_nascimento
             }
 
-            with open("dic_clientes.txt", "w") as file:
-                file.write(str(dic_cliente))
+            with open("clientes.json", "w") as file:
+                json.dump(clientes, file, indent=4)
         else:
             raise 'Digite um número de CPF com 11 digitos!'
 
     def busca_cliente_por_cpf(self, cpf: str):
         if cpf.isnumeric():
-            dic_cliente = self.read_cliente()
-            if dic_cliente[cpf] is not None:
-                cliente = dic_cliente[cpf]
-                dados_cliente =  f"Dados do cliente:\nCPF: {cpf}\nNome: {cliente['nome']}\nSobrenome: {cliente['sobrenome']}\nData de Nascimento: {cliente['data de nascimento']}"
-                return dados_cliente
+            clientes = self.read_cliente()
+            if clientes[cpf] is not None:
+                cliente = clientes[cpf]
+                return cliente
             else:
                 raise 'O CPF digitado não existe!'        
         else:
             raise 'Digite um número de CPF com 11 digitos!'        
 
     def clientes_ordem_alfabetica(self):
-        dic_cliente = self.read_cliente()
-        sorted_clientes = dict(sorted(dic_cliente.items(), key=lambda item: item[1]['nome']))
-        index = 1
-        for cpf, dados in sorted_clientes.items():
-            info = f"Dados do Cliente {index}:\n"
-            info += f"CPF: {cpf}\n"
-            info += f"Nome: {dados['nome']}\n"
-            info += f"Sobrenome: {dados['sobrenome']}\n"
-            info += f"Data de Nascimento: {dados['data de nascimento']}\n"
-            index += 1
-            print(info)
+        clientes = self.read_cliente()
+        sorted_clientes = dict(sorted(clientes.items(), key=lambda item: item[1]['nome']))
+        return sorted_clientes
 
 pedro = Cliente("123456789", "Pedro", "Ferrari", "10/10/1998")
-
 thiago = Cliente("987654321", "Thiago", "Anamastor", "21/12/1976")
 
-thiago.clientes_ordem_alfabetica()
+print(thiago.clientes_ordem_alfabetica())
+print(thiago.busca_cliente_por_cpf("123456789"))
